0e18c70f00a0175d9976cf33caa8b25d52bc49d0
Pavan Kurma Rao Koka <pavankurmarao.k@AR-IN-M-150.local>
2026-07-04 10:43:00 +0530

    Repo init

--- src/analyse/plasmaGL.js
+++ src/analyse/plasmaGL.js
@@ -185,7 +185,11 @@ export function initGL(canvas, config = {}) {
 
   const buf = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buf);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, 3, -1, -1, 3]), gl.STATIC_DRAW);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([-1, -1, 3, -1, -1, 3]),
+    gl.STATIC_DRAW
+  );
   const loc = gl.getAttribLocation(program, "a_pos");
   gl.enableVertexAttribArray(loc);
   gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
